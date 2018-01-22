dcb77f345429169658a31ed2202088dfe9ca3710
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2018-01-22 09:39:27 -0500

    specify the message digest for openssl

--- fins/funs.d/encryption.zsh
+++ fins/funs.d/encryption.zsh
@@ -1,9 +1,12 @@
+# It is important to specify the message digest (-md) for portability:
+# the default digest was changed from MD5 to SHA256 in Openssl 1.1.
+
 encrypt-file () {
     file=$1
-    openssl aes-256-cbc -in "$file" -out "$file.encrypted"
+    openssl aes-256-cbc -md sha256 -in "$file" -out "$file.encrypted"
 }
 
 decrypt-file () {
     file=$1
-    openssl aes-256-cbc -d -in "$file" -out "${file%.encrypted}"
+    openssl aes-256-cbc -md sha256 -d -in "$file" -out "${file%.encrypted}"
 }
