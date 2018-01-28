ea9712f6f2a1c61fca61c6942fc31e779ee487f1
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2018-01-29 02:18:39 +0530

    use hyphens in emacs.zsh function names

--- fins/funs.d/emacs.zsh
+++ fins/funs.d/emacs.zsh
@@ -1,4 +1,4 @@
-__emacs_echo () {
+__emacs-echo () {
     # only works since zsh 5.3
     # if [[ -v EMACS_ECHO ]]; then
     # would also work
@@ -12,98 +12,101 @@ __emacs_echo () {
     fi
 }
 
-__emacs_daemon_socket () {
+__emacs-daemon-socket () {
     echo /tmp/emacs$(id --user)/server
 }
 
-__emacs_daemon_socket_exists () {
-    test -S $(__emacs_daemon_socket)
+__emacs-daemon-socket-exists () {
+    test -S $(__emacs-daemon-socket)
 }
 
-__emacs_daemon_socket_is_open () {
-    __emacs_echo 'Skip the socket open test because it takes 2s'
-    true || lsof -w $(__emacs_daemon_socket) > /dev/null
+__emacs-daemon-socket-is-open () {
+    # FIXME
+    __emacs-echo 'Skip the socket open test because it takes 2s'
+    true || lsof -w $(__emacs-daemon-socket) > /dev/null
 }
 
-__emacs_daemon_is_up () {
-    __emacs_daemon_socket_exists && __emacs_daemon_socket_is_open
+__emacs-daemon-is-up () {
+    __emacs-daemon-socket-exists && __emacs-daemon-socket-is-open
 }
 
-__emacs_X_frame_exists () {
-    xprop -name emacs_X_frame > /dev/null 2>&1
+# FIXME make the name a variable in .home.d
+__emacs-X-frame-exists () {
+    xprop -name emacs-X-frame > /dev/null 2>&1
 }
 
-__emacs_daemon_start () {
-    if ! $(__emacs_daemon_is_up); then
-        if $(__emacs_daemon_socket_exists); then
-            __emacs_echo 'Removing stale socket'
-            rm -f $(__emacs_daemon_socket)
+__emacs-daemon-start () {
+    if ! $(__emacs-daemon-is-up); then
+        if $(__emacs-daemon-socket-exists); then
+            __emacs-echo 'Removing stale socket'
+            rm -f $(__emacs-daemon-socket)
         fi
-        __emacs_echo 'Starting emacs daemon'
+        __emacs-echo 'Starting emacs daemon'
         (cd $HOME && emacs --daemon --eval '(setq frame-title-format "emacs_X_frame")')
     else
-        __emacs_echo 'Emacs daemon is already running'
+        __emacs-echo 'Emacs daemon is already running'
     fi
 }
 
-__emacs_daemon_stop () {
-    if $(__emacs_daemon_is_up); then
-        __emacs_echo 'Stopping emacs daemon'
+__emacs-daemon-stop () {
+    if $(__emacs-daemon-is-up); then
+        __emacs-echo 'Stopping emacs daemon'
         emacsclient --eval '(progn (save-some-buffers t) (kill-emacs))'
     else
-        __emacs_echo 'Emacs daemon is not running'
+        __emacs-echo 'Emacs daemon is not running'
     fi
 }
 
-__emacs_daemon_restart () {
-    __emacs_daemon_stop
-    __emacs_daemon_start
+__emacs-daemon-restart () {
+    __emacs-daemon-stop
+    __emacs-daemon-start
 }
 
 __emacsclient () {
+    # FIXME: it does not always work
     emacsclient --eval '(define-key input-decode-map [?\C-m] [C-m])'
     emacsclient "$@"
 }
 
-__emacsclient_in_X_frame () {
-    if $(__emacs_X_frame_exists); then
-        __emacs_echo 'Attaching to existing X frame'
+__emacsclient-in-X-frame () {
+    if $(__emacs-X-frame-exists); then
+        __emacs-echo 'Attaching to existing X frame'
         __emacsclient "$@"
     else
-        __emacs_echo 'Starting X frame'
+        __emacs-echo 'Starting X frame'
         __emacsclient --create-frame "$@"
     fi
 }
 
-__emacsclient_in_X_frame_no_wait () {
-    __emacsclient_in_X_frame --no-wait "$@"
+__emacsclient-in-X-frame-no-wait () {
+    __emacsclient-in-X-frame --no-wait "$@"
 }
 
 
 emacs-attach () {
-    __emacs_daemon_start
-    __emacsclient_in_X_frame_no_wait "$@"
+    __emacs-daemon-start
+    __emacsclient-in-X-frame-no-wait "$@"
 }
 
 emacs-wait () {
-    __emacs_daemon_start
-    __emacsclient_in_X_frame "$@"
+    __emacs-daemon-start
+    __emacsclient-in-X-frame "$@"
 }
 
 emacs-kill () {
-    __emacs_daemon_stop
+    __emacs-daemon-stop
 }
 
 emacs-restart () {
-    __emacs_daemon_restart
-    __emacsclient_in_X_frame_no_wait "$@"
+    __emacs-daemon-restart
+    __emacsclient-in-X-frame-no-wait "$@"
 }
 
 magit () {
     (if [[ $# == 1 ]]; then
          cd $1;
      fi
-     __emacs_daemon_start
-     __emacsclient_in_X_frame_no_wait --eval '(magit-status)'
+     __emacs-daemon-start
+     __emacsclient-in-X-frame-no-wait --eval '(magit-status)'
     )
 }
