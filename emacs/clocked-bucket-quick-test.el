872ff7feb7255fc7311e969bf2a29ec9a382a5f1
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2018-03-18 15:52:15 -0400

    improve quick-test

--- emacs/clocked-bucket-quick-test.el
+++ emacs/clocked-bucket-quick-test.el
@@ -1,7 +1,8 @@
-(require 'clocked-bucket "/home/ddb/.home.d/emacs/clocked-bucket.el")
+(load "/home/ddb/.home.d/emacs/clocked-bucket.el")
 (require 'home.d-org "/home/ddb/.home.d/emacs/home.d-org.el")
 
 (defun quick-test ()
+  (interactive)
   (find-file "/home/ddb/.home.d/emacs/clocked-bucket-tests.org")
   (total-buckets
     (list
@@ -14,5 +15,3 @@
     '((:A . "Allocation A")
       (:B . "Allocation B")))
   (bury-buffer))
-
-(quick-test)
