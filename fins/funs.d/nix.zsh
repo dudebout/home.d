8c1260b5c55816bcecf2b087fd8d37cd8690a7df
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2018-10-19 06:26:39 -0400

    add nix fins

--- fins/funs.d/nix.zsh
+++ fins/funs.d/nix.zsh
@@ -5,3 +5,16 @@ nix-ls () {
 nix-which () {
   readlink $(which "$@")
 }
+
+nx () {
+    branch="$1"
+    profile="$2"
+
+    nix_dir=$HOME/nix
+
+    nix-env --file $nix_dir/pkgs/$branch -p $nix_dir/profiles/$profile "${@:3}"
+}
+
+nix-upgrade-home.d () {
+    nix-env --remove-all --install --from-expression "import $HOME/.config/nixpkgs/home.d.nix"
+}
