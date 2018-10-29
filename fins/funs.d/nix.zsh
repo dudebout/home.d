0f4e66a6e2f778c68ed21808445e928d4aaacadc
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2018-10-28 21:22:53 -0400

    improve nix fins

--- fins/funs.d/nix.zsh
+++ fins/funs.d/nix.zsh
@@ -2,19 +2,6 @@ nix-ls () {
   ls --color=always /nix/store | sed -r 's/([a-z0-9]{32})-([-.a-zA-Z0-9]+)(.*)$/\2 (\1)\3/'
 }
 
-nix-which () {
-  readlink $(which "$@")
-}
-
-nx () {
-    branch="$1"
-    profile="$2"
-
-    nix_dir=$HOME/nix
-
-    nix-env --file $nix_dir/pkgs/$branch -p $nix_dir/profiles/$profile "${@:3}"
-}
-
 nix-upgrade-home.d () {
-    nix-env --remove-all --install --from-expression "import $HOME/.config/nixpkgs/home.d.nix"
+    nix-env --file '<nixos-18_09>' --remove-all --install --from-expression "import $HOME/.config/nixpkgs/home.d.nix"
 }
