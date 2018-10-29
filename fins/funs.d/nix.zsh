3a59dfa5bfb61202df380a3f64b483dafe5a81a1
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2018-10-29 07:54:48 -0400

    add nix-ag

--- fins/funs.d/nix.zsh
+++ fins/funs.d/nix.zsh
@@ -1,5 +1,10 @@
 nix-ls () {
-  ls --color=always /nix/store | sed -r 's/([a-z0-9]{32})-([-.a-zA-Z0-9]+)(.*)$/\2 (\1)\3/'
+    ls --color=always /nix/store | sed -r 's/([a-z0-9]{32})-([-.a-zA-Z0-9]+)(.*)$/\2 (\1)\3/'
+}
+
+nix-ag () {
+    cd $HOME/.nix-defexpr/channels/nixpkgs
+    ag "$@"
 }
 
 nix-upgrade-home.d () {
