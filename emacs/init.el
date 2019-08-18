2219b4dce401a89431cdf03031061fcaf817910d
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2019-08-18 10:03:28 -0400

    fix typo

--- emacs/init.el
+++ emacs/init.el
@@ -29,7 +29,7 @@
               require-final-newline t)
 
 (add-hook 'before-save-hook #'whitespace-cleanup)
-(add-hook 'before-save-hook #'home.d/org-evaluate-all-time-ranges)
+(add-hook 'before-save-hook #'home.d/org-evaluate-buffer-time-ranges)
 (add-hook 'after-save-hook #'executable-make-buffer-file-executable-if-script-p)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
