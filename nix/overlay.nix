a5e696d4b6e80612807c7238fd253fc0a6cbc284
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2020-01-04 10:49:28 -0500

    use default.nix from retrospect's repository

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,28 +1,6 @@
 self: super:
 {
   emacsPackages = super.emacsPackages.overrideScope' (_self: _super: {
-    retrospect = self.callPackage ({ dash
-                                   , fetchFromGitHub
-                                   , lib
-                                   , org-plus-contrib
-                                   , melpaBuild }:
-      melpaBuild {
-        pname = "retrospect";
-        ename = "retrospect";
-        version = "0.1";
-        src = (import nix/sources.nix).retrospect;
-        recipe = builtins.toFile "retrospect-recipe" ''
-          (retrospect
-              :fetcher github
-              :repo "dudebout/retrospect")
-        '';
-        packageRequires = [ dash org-plus-contrib ];
-        meta = {
-          homepage = https://github.com/dudebout/retrospect;
-          license = lib.licenses.gpl3;
-        };
-      }) { inherit (self) lib fetchFromGitHub;
-           inherit (self.emacsPackages) melpaBuild dash org-plus-contrib;
-         };
+    retrospect = self.callPackage ((import nix/sources.nix).retrospect) { pkgs = self; };
   });
 }
