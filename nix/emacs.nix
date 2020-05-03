26fd69dca858b7f9618c071ed75831be5bf24da6
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2020-05-03 09:53:08 -0400

    add cmake-mode package

--- nix/emacs.nix
+++ nix/emacs.nix
@@ -24,6 +24,7 @@ emacsPackages.emacsWithPackages (ePkgs: with ePkgs;
     company-coq
     counsel
     counsel-projectile
+    cmake-mode
     elisp-slime-nav
     fill-column-indicator
     git-gutter
