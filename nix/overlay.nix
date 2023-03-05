11f5f7892906f95b676b69dd4078b729ca4db126
Nicolas Dudebout <nicolas.dudebout@gmail.com>
2023-03-05 08:50:57 -0500

    Remove unused configs

--- nix/overlay.nix
+++
@@ -1,6 +0,0 @@
-self: super:
-{
-  emacsPackages = super.emacsPackages.overrideScope' (_self: _super: {
-    retrospect = self.callPackage ((import nix/sources.nix).retrospect) { pkgs = self; };
-  });
-}
